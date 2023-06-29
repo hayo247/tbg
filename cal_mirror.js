82f1771d77727be3fc9ddd8014a4403e00ec4744
hayo247 <137261401+hayo247@users.noreply.github.com>
2023-06-30 04:40:10 +0900

    Update cal_mirror.js

--- cal_mirror.js
+++ cal_mirror.js
@@ -9,6 +9,7 @@ $(function(){
 	$('#loading').hide(); //첫 시작시 로딩바를 숨겨준다.
 	
 	if(isMobile()){
+		$('head').append('<meta http-equiv="pragma" content="no-cache">');
 		$("body").addClass('mobile');
 		$("#txt_glassSort").click(function(){
 			$("#ul_glassSort").show();
